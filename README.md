9b40cb42ec62aecfc84d781f70c1303ff504c869
leeseoeun <seoeun.shuen@gmail.com>
2024-07-25 18:32:01 +0900

    0725 미들러 Day 4

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # 99club
+
+<b>Arrays.sort(배열)</b>
+- 배열을 리스트로 변환할 필요 없이 정렬할 수 있음
+
+<b>Arrays.sort(배열, Comparator 구현체)</b>
+- Arrays.sort(배열, new Comparator<String>() {}) 형태로 사용
+- `Comparator` 인터페이스에는 `compare` 메소드가 존재
+- `compare` 메소드
+    - |리턴||
+        |-|-|
+        |1|o1 > o2|
+        |0|o1 = o2|
+        |-1|o1 < o2|
+
+<b>compareTo</b>
+- o1.compareTo(o2) 형태로 사용
+- 문자열, 숫자 비교
+
+<b>Character.compare(char, char)</b>
+- char 비교
---
+++ app/src/main/java/club/middler/Day4_JadenCase_문자열_만들기.java
@@ -0,0 +1,50 @@
+package club.middler;
+
+import java.util.Scanner;
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/12951
+public class Day4_JadenCase_문자열_만들기 {
+
+  public static String solution(String s) {
+    String answer = "";
+
+    String[] ss = s.split("");
+    answer += ss[0].toUpperCase();
+    for (int i = 1; i < ss.length; i++) {
+      // 첫글자
+      if (ss[i - 1].equals(" ") && !ss[i].equals(" ")) {
+        answer += ss[i].toUpperCase();
+
+        // 연속 공백
+      } else if (ss[i - 1].equals(" ") && ss[i].equals(" ")) {
+        answer += " ";
+
+        // 나머지 글자
+      } else {
+        answer += ss[i].toLowerCase();
+      }
+    }
+
+    return answer;
+
+    /*
+     * String[] sp = s.toLowerCase().split("");
+     * boolean flag = true;
+     * 
+     * for(String ss : sp) {
+     *  answer += flag ? ss.toUpperCase() : ss;
+     *  flag = ss.equals(" ") ? true : false;
+     * }
+     * */
+  }
+
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    String s = sc.next();
+
+    System.out.println(solution(s));
+
+  }
+
+}

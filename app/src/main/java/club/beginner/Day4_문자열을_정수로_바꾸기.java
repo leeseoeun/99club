39a3a65e723d5bbfaf06772e00fca79a69c67247
leeseoeun <seoeun.shuen@gmail.com>
2024-07-25 18:40:56 +0900

    0725 비기너 Day 4

--- app/src/main/java/club/beginner/Day4_문자열을_정수로_바꾸기.java
+++ app/src/main/java/club/beginner/Day4_문자열을_정수로_바꾸기.java
@@ -1,9 +1,30 @@
 package club.beginner;
 
+import java.util.Scanner;
+
 public class Day4_문자열을_정수로_바꾸기 {
 
+  public static int solution(String s) {
+    int answer = 0;
+
+    String sign = s.substring(0, 1);
+    if (sign.equals("+")) {
+      answer = Integer.parseInt(s.substring(1));
+    } else if (sign.equals("-")) {
+      answer -= Integer.parseInt(s.substring(1));
+    } else {
+      answer = Integer.parseInt(s);
+    }
+
+    return answer;
+  }
+
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
+
+    Scanner sc = new Scanner(System.in);
+    String s = sc.next();
+
+    System.out.println(solution(s));
 
   }
 

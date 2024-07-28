c934440a2c26e51a7c70542bc981c482ad19a963
leeseoeun <seoeun.shuen@gmail.com>
2024-07-29 06:14:17 +0900

    .

---
+++ app/src/main/java/club/middler/Day7_하노이의_탑.java
@@ -0,0 +1,24 @@
+package club.middler;
+
+import java.util.Scanner;
+
+public class Day7_하노이의_탑 {
+
+  public static int[][] solution(int n) {
+    int[][] answer = {};
+
+
+
+    return answer;
+  }
+
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+
+    System.out.println(solution(n));
+
+  }
+
+}

3114b40d4cab961b78fa860b2fcc1e38166ead02
leeseoeun <seoeun.shuen@gmail.com>
2024-08-04 03:32:46 +0900

    0804 미들러 Day 13

---
+++ app/src/main/java/club/middler/Day13_숫자_카드.java
@@ -0,0 +1,36 @@
+package club.middler;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Day13_숫자_카드 {
+
+  public static void main(String[] args) throws NumberFormatException, IOException {
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    // 상근이 숫자 카드
+    int number = Integer.parseInt(br.readLine());
+    int[] sgArr = new int[number];
+
+    String str = br.readLine();
+    StringTokenizer st = new StringTokenizer(str);
+    for (int i = 0; i < number; i++) {
+      sgArr[i] = Integer.parseInt(st.nextToken());
+    }
+
+    // 숫자 카드
+    number = Integer.parseInt(br.readLine());
+    int[] numberArr = new int[number];
+
+    str = br.readLine();
+    st = new StringTokenizer(str);
+    for (int i = 0; i < number; i++) {
+      numberArr[i] = Integer.parseInt(st.nextToken());
+    }
+
+  }
+
+}

bc774981a42fb2f49856b9bb20dee474cbb62880
leeseoeun <seoeun.shuen@gmail.com>
2024-08-06 15:56:57 +0900

    .

---
+++ app/src/main/java/club/middler/Day16_모음사전.java
@@ -0,0 +1,19 @@
+package club.middler;
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/84512
+public class Day16_모음사전 {
+
+  public static int solution(String word) {
+    int answer = 0;
+    return answer;
+  }
+
+  public static void main(String[] args) {
+
+    String word = "AAAAE";
+
+    System.out.println(solution(word));
+
+  }
+
+}

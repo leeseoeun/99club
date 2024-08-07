899f0a74f11815929b0a5a9234735aeff2bedc1f
leeseoeun <seoeun.shuen@gmail.com>
2024-08-07 17:58:41 +0900

    0807 미들러 Day 16

--- app/src/main/java/club/middler/Day16_모음사전.java
+++ app/src/main/java/club/middler/Day16_모음사전.java
@@ -1,10 +1,38 @@
 package club.middler;
 
+import java.util.ArrayList;
+import java.util.List;
+
 // https://school.programmers.co.kr/learn/courses/30/lessons/84512
 public class Day16_모음사전 {
 
+  static String[] alphabet = {"A", "E", "I", "O", "U"};
+  static List<String> list = new ArrayList<String>();
+  static int idx = 0;
+
+  private static void addAlphabet(String currentWord, int length) {
+    if (length > 5) {
+      return;
+    }
+
+    list.add(currentWord);
+
+    for (int i = 0; i < alphabet.length; i++) {
+      addAlphabet(currentWord + alphabet[i], length + 1);
+    }
+  }
+
   public static int solution(String word) {
     int answer = 0;
+
+    addAlphabet("", 0);
+
+    for (int i = 0; i < list.size(); i++) {
+      if (list.get(i).equals(word)) {
+        answer = i;
+      }
+    }
+
     return answer;
   }
 
---
+++ app/src/main/java/club/middler/Day17_촌수계산.java
@@ -0,0 +1,11 @@
+package club.middler;
+
+// https://www.acmicpc.net/problem/2644
+public class Day17_촌수계산 {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+
+}

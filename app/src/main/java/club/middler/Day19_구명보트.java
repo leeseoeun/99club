addd59a307ac9ba87a0846f41024209c2fc34887
leeseoeun <seoeun.shuen@gmail.com>
2024-08-10 02:34:37 +0900

    0810 미들러 Day 19

---
+++ app/src/main/java/club/middler/Day19_구명보트.java
@@ -0,0 +1,40 @@
+package club.middler;
+
+import java.util.Arrays;
+
+public class Day19_구명보트 {
+
+  public static int solution(int[] people, int limit) {
+    int answer = 0;
+
+    // 투 포인터
+    int left = 0;
+    int right = people.length - 1;
+
+    Arrays.sort(people);
+
+    // 두 포인터가 만날 때까지
+    while (left <= right) {
+      if (people[left] + people[right] <= limit) {
+        left++;
+      }
+
+      right--; // 무거운 큰 사람만 태우기
+
+      answer++;
+    }
+
+    return answer;
+  }
+
+  public static void main(String[] args) {
+
+    //    int[] people = {70, 50, 80, 50};
+    int[] people = {70, 80, 50};
+    int limit = 100;
+
+    System.out.println(solution(people, limit));
+
+  }
+
+}

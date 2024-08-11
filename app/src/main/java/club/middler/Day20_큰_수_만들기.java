d2f378d7b230a62c2d92f45b82103a25aed71b36
leeseoeun <seoeun.shuen@gmail.com>
2024-08-12 00:24:01 +0900

    .

---
+++ app/src/main/java/club/middler/Day20_큰_수_만들기.java
@@ -0,0 +1,22 @@
+package club.middler;
+
+public class Day20_큰_수_만들기 {
+
+  public static String solution(String number, int k) {
+    String answer = "";
+
+
+
+    return answer;
+  }
+
+  public static void main(String[] args) {
+
+    String number = "1924";
+    int k = 2;
+
+    System.out.println(solution(number, k));
+
+  }
+
+}

6c1e2030cc2ff3b8c73d25fc5da95a44bb6ac72d
leeseoeun <seoeun.shuen@gmail.com>
2024-08-19 15:28:43 +0900

    0819 미들러 Day 28

--- app/src/main/java/club/middler/Day28_괄호_회전하기.java
+++ app/src/main/java/club/middler/Day28_괄호_회전하기.java
@@ -1,15 +1,43 @@
 package club.middler;
 
+import java.util.Stack;
+
 // https://school.programmers.co.kr/learn/courses/30/lessons/76502
 public class Day28_괄호_회전하기 {
 
   public static int solution(String s) {
     int answer = 0;
 
-    String[] ss = s.split("");
-    for (int i = 0; i < ss.length; i++) {
-      String newS = s.substring(i) + s.substring(0, i);
-      System.out.println(newS);
+    String newS = "";
+    Stack<Character> stack = new Stack<>();
+    boolean flag = true;
+
+    for (int i = 0; i < s.length(); i++) {
+      newS = s.substring(i) + s.substring(0, i);
+      stack.clear();
+      flag = true;
+
+      for (char c : newS.toCharArray()) {
+        if (c == '(' || c == '[' || c == '{') {
+          stack.push(c);
+        } else {
+          if (stack.isEmpty()) {
+            flag = false;
+            break;
+          }
+
+          char top = stack.pop();
+          if ((c == ')' && top != '(') ||
+              (c == ']' && top != '[') ||
+              (c == '}' && top != '{')) {
+            flag = false;
+          }
+        }
+      }
+
+      if (flag && stack.isEmpty()) {
+        answer++;
+      }
     }
 
     return answer;
